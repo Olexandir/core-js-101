58a4709256c938e624184583865f3e626962f3bf
Olexandir <68996102+Olexandir@users.noreply.github.com>
2020-12-14 13:59:45 +0300

    Summary
    writing functions

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,8 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe( /* isPositiveAnswer */ ) {
+    throw new Error('Not implemented');
 }
 
 
@@ -48,8 +48,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+    return Promise.all(array);
 }
 
 /**
@@ -71,8 +71,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+    return Promise.race(array);
 }
 
 /**
@@ -92,13 +92,13 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+    throw new Error('Not implemented');
 }
 
 module.exports = {
-  willYouMarryMe,
-  processAllPromises,
-  getFastestPromise,
-  chainPromises,
+    willYouMarryMe,
+    processAllPromises,
+    getFastestPromise,
+    chainPromises,
 };
--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+    return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+    return (x) => x ** exponent;
 }
 
 
@@ -63,7 +63,7 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom()      => null
  */
 function getPolynom() {
-  throw new Error('Not implemented');
+    throw new Error('Not implemented');
 }
 
 
@@ -81,8 +81,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+    const random = func();
+    return () => random;
 }
 
 
@@ -101,8 +102,8 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+    throw new Error('Not implemented');
 }
 
 
@@ -129,8 +130,8 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+    throw new Error('Not implemented');
 }
 
 
@@ -147,8 +148,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+    throw new Error('Not implemented');
 }
 
 
@@ -169,18 +170,22 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+    let data = startFrom - 1;
+    return () => {
+        data += 1;
+        return data;
+    };
 }
 
 
 module.exports = {
-  getComposition,
-  getPowerFunction,
-  getPolynom,
-  memoize,
-  retry,
-  logger,
-  partialUsingArguments,
-  getIdGeneratorFunction,
+    getComposition,
+    getPowerFunction,
+    getPolynom,
+    memoize,
+    retry,
+    logger,
+    partialUsingArguments,
+    getIdGeneratorFunction,
 };
